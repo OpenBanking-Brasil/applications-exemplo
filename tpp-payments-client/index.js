5a10b981323856291c7c1715a00c9249afe0bd7d
Bernardo Campos <bernardo.campos@raidiam.com>
2022-02-11 20:01:30 -0300

    Removed length from setupLog

--- tpp-payments-client/index.js
+++ tpp-payments-client/index.js
@@ -103,13 +103,12 @@ const config = require('./config');
 
 
   const TotalBanks = axiosResponse.data;
-  setupLog(TotalBanks.length)
   const availableBanks = TotalBanks.filter(e => 
     e.AuthorisationServers.some(as =>
       as.ApiResources.some(apifamily => 
         apifamily.ApiFamilyType == "payments-consents")
     ))
-  setupLog(availableBanks.length);
+  setupLog(availableBanks);
 
 
   //These are set as global variables, they should be stored in a memory cache and retrieved based on the users session / state

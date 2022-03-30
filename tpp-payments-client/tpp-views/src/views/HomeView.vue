35cfdae0e9aae347da8f4fc9877f18649d0e1ac0
Bernardo Campos <bernardo.campos@raidiam.com>
2022-03-30 19:10:01 -0300

    Updated homeview

--- tpp-payments-client/tpp-views/src/views/HomeView.vue
+++ tpp-payments-client/tpp-views/src/views/HomeView.vue
@@ -18,6 +18,89 @@
             </div>
               <v-spacer/>
             </v-app-bar>
+            <v-row class="pt-12">
+            </v-row >
+            <v-row class="pt-12">
+            </v-row >
+            <v-row class="pt-12">
+            <v-col>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          Phase 2
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Customer Data
+        </v-list-item-title>
+        <v-list-item-subtitle>This is still a work-in-progress</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        
+        size="40"
+      ><v-icon
+      large
+      color="grey darken-2"
+    >
+      mdi-swap-horizontal
+    </v-icon>
+      
+      </v-list-item-avatar>
+    </v-list-item>
+
+  </v-card>
+            </v-col>
+
+            <v-col>
+              <v-card
+    class="mx-auto"
+    max-width="344"
+    
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          Phase 3
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Payments
+        </v-list-item-title>
+        <v-list-item-subtitle>Use the Mock TPP for Phase 3 Payments</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        
+        size="40"
+      ><v-icon
+      large
+      color="green darken-2"
+    >
+      mdi-cash-fast
+    </v-icon>
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+        @click="$router.push('banks')"
+      >
+        Go
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+            </v-col>
+
+            </v-row>
 
             </v-sheet>
       </v-col>
